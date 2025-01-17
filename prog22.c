6a778902a8ef7b9b92447c1d2ae0ccb4bf705b67
dziezak <01189082@pw.edu.pl>
2025-01-17 22:20:03 +0100

    nie skonczone zadanie prog22.c

---
+++ prog22.c
@@ -0,0 +1,22 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+#define BUFFER_SIZE 256
+#define READCHUNKS 4
+#define THREAD_NUM 3
+
+
+volatile sig_atomic_t work = 1;
+
+typedef struct{
+    
+}
